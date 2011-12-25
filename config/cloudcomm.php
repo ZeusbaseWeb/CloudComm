c1ecadff0d4f0aa46f4586eb009250e6d32937aa
John Schrom <john@john.mn>
2011-12-25 13:11:58 -0600

    initial commit

---
+++ config/cloudcomm.php
@@ -0,0 +1,11 @@
+<?php
+
+# Phaxio - Faxes
+$config['phaxio_base'] = 'https://api.phaxio.com/v1/';
+$config['phaxio_key'] = 'YOUR PHAXIO KEY';
+$config['phaxio_secret'] = 'YOUR PHAXIO SECRET';
+
+# Twilio - SMS, Voice
+$config['twilio_version'] = '2010-04-01';
+$config['twilio_sid'] = 'YOUR SID';
+$config['twilio_token'] = 'YOUR TOKEN';
